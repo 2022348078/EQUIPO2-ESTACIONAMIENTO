9fdbb90ff537358f86ef69c5e81464f454258958
2022348078 <2022348078@uteq.edu.mx>
2026-08-17 22:09:51 -0600

    Implementar libreria para sensores IR

---
+++ ESTACIONAMIENTO_STM32/Inc/sensores.h
@@ -0,0 +1,13 @@
+/*
+ * sensores.h
+ *
+ *  Created on: Aug 17, 2026
+ *      Author: pollo
+ */
+
+#ifndef SENSORES_H_
+#define SENSORES_H_
+
+
+
+#endif /* SENSORES_H_ */
---
+++ ESTACIONAMIENTO_STM32/Src/sensores.c
@@ -0,0 +1,10 @@
+/*
+ * sensores.c
+ *
+ *  Created on: Aug 17, 2026
+ *      Author: pollo
+ */
+
+
+
+
