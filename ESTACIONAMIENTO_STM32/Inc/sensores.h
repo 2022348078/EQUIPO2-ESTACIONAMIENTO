3fddeb1f81c7327cb14eba8f4262e8f122a31a08
2022348078 <2022348078@uteq.edu.mx>
2026-08-18 00:26:32 -0600

    Implementar sensores IR de entrada y salida
    Configuración de dos sensores infrarrojos en PE0 y PE1 para detectar vehículos en la entrada y salida, utilizando LEDs indicadores para verificar su funcionamiento.

--- ESTACIONAMIENTO_STM32/Inc/sensores.h
+++ ESTACIONAMIENTO_STM32/Inc/sensores.h
@@ -1,13 +1,11 @@
-/*
- * sensores.h
- *
- *  Created on: Aug 17, 2026
- *      Author: pollo
- */
-
 #ifndef SENSORES_H_
 #define SENSORES_H_
 
+#include <stdint.h>
 
+void Sensores_Init(void);
+
+uint8_t Sensor_Entrada(void);
+uint8_t Sensor_Salida(void);
 
 #endif /* SENSORES_H_ */
